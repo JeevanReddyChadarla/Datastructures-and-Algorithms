cb408d344f5d30fc1e94c0f377967b5947368a02
Jeevan <gonereddy1729@gmail.com>
2023-12-25 23:16:43 +0530

    add map prob statements

---
+++ src/main/java/Map/problemToSolve.java
@@ -0,0 +1,10 @@
+package Map;
+
+public class problemToSolve {
+//	https://leetcode.com/problems/next-greater-element-i/description/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/Map/reduceArraySizeToHald1338.java
@@ -0,0 +1,34 @@
+package Map;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class reduceArraySizeToHald1338 {
+	
+//	https://leetcode.com/problems/reduce-array-size-to-the-half/solutions/
+
+//	public static int minSetSize(int[] arr) {
+//		Arrays.sort(arr);
+//        HashSet<Integer>set=new HashSet<Integer>();
+//        int start=0, end=arr.length-1, count=0, m=0;
+//        while(start<=end){
+//           if(set.contains(arr[start])) {
+//        	   count++;
+//           }
+//        	   set.add(arr[start]);
+//        	   if(set.size()+count>=(end+1)/2) {
+//        		   return set.size();
+//        	   }
+//           start++;
+//        }
+//        return set.size();
+//    }
+
+	public static void main(String[] args) {
+//		int arr[] = {9,77,63,22,92,9,14,54,8,38,18,19,38,68,58,19};//{9,77,63,22,92,9,14,54,8,38,18,19,38,68,58,19};
+//		int len=arr.length;
+//		System.out.println(minSetSize(arr));
+
+	}
+
+}
